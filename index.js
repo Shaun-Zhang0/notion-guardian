39ba4fd56d67cc344fd722b58d8369013131610d
ZhangXiaofeng <47142236+Shaun-Zhang0@users.noreply.github.com>
2023-02-10 15:40:08 +0800

    Update index.js

--- index.js
+++ index.js
@@ -85,6 +85,75 @@ const exportFromNotion = async (destination, format) => {
   });
 };
 
+/**
+ * 获取文件名的原始名称
+ * @param fileName
+ */
+function getFileOriginName(fileName) {
+    if (fileName.length <= 33) {
+        return fileName;
+    }
+    const hashLength = 33;
+    const pattern = /\.{1}[A-Za-z]{1,}$/;
+    const _suffixIndex = pattern.exec(fileName).index
+    const _suffix = pattern.exec(fileName)[0];
+    console.log(fileName.substring(0, _suffixIndex - hashLength) + _suffix)
+    return fileName.substring(0, _suffixIndex - hashLength) + _suffix;
+}
+
+function travel(dir, callback) {
+
+    fs.readdirSync(dir).forEach((file) => {
+        const pathname = path.join(dir, file)
+
+        let newFilePath = ''
+        console.log(newFilePath)
+        if (fs.statSync(pathname).isDirectory()) {
+            newFilePath = path.join(dir, file.slice(0, -33));
+            travel(pathname, callback)
+        } else {
+            newFilePath = path.join(dir, getFileOriginName(file));
+            callback(file)
+        }
+        renameFileOrDirectory(pathname, newFilePath)
+    })
+}
+
+/**
+ * 因为压缩解压后会带hash 此方法用于删除export根目录的hash值
+ * 方便github的action可以找到对应的文件夹
+ */
+function renameExportDirName() {
+    const dir = './workspace';
+    fs.readdirSync('./workspace').forEach((file) => {
+        const pathname = path.join(dir, file)
+        const newPathName = path.join(dir, 'export')
+        if (fs.statSync(pathname).isDirectory()) {
+            renameFileOrDirectory(pathname, newPathName)
+        }
+    })
+}
+
+
+/**
+ * 重命名文件夹或文件
+ * @param filePath - 原命名文件路径
+ * @param newFilePath - 新命名文件路径
+ */
+function renameFileOrDirectory(filePath, newFilePath) {
+    try {
+        fs.renameSync(filePath, newFilePath)
+        console.log("Successfully renamed the file!")
+    } catch (err) {
+        throw err
+    }
+}
+function renameAllFile() {
+    renameExportDirName();
+    travel('./workspace/export', function (pathname) {
+        // console.log(pathname)
+    });
+}
 const run = async () => {
   const workspaceDir = join(process.cwd(), `workspace`);
   const workspaceZip = join(process.cwd(), `workspace.zip`);
@@ -94,6 +163,7 @@ const run = async () => {
   await mkdir(workspaceDir, { recursive: true });
   await extract(workspaceZip, { dir: workspaceDir });
   await unlink(workspaceZip);
+  renameAllFile();
 
   console.log(`✅ Export downloaded and unzipped.`);
 };
