dd5503dd03c3132476025fab20b0e2be10cf1393
ZhangXiaofeng <47142236+Shaun-Zhang0@users.noreply.github.com>
2023-02-25 17:54:26 +0800

    Update index.js

--- index.js
+++ index.js
@@ -103,6 +103,7 @@ function getFileOriginName(fileName) {
 
 const rewriteMarkdownImgOrLink = function (filePath, sourceRegx, targetStr) {
     const data = fs.readFileSync(filePath, {flag: 'r+'})
+    console.log(filePath)
     let str = data.toString();
     str = str.replace(/(!\[[a-zA-Z0-9_-\u4e00-\u9fa5,\.\[\]\(\)\{\}]+\]\([a-zA-Z0-9_-\u4e00-\u9fa5,\.\[\]\(\)\{\}]+)%[a-zA-Z-0-9]+\//, `$1/`);
     fs.writeFileSync(filePath, str, {flag: 'w+'});
